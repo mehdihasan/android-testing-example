605b49446ea2987ffac695b066a56fe61d3a8e98
mehdihasan <email2mehdihasan@gmail.com>
2019-05-11 20:27:35 +0200

    Login activity ui test improved

--- app/src/androidTest/java/me/mehdihasan/testingexample/LoginActivityActionTest.java
+++ app/src/androidTest/java/me/mehdihasan/testingexample/LoginActivityActionTest.java
@@ -14,6 +14,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -65,6 +66,7 @@ public class LoginActivityActionTest {
 
         // Check that the result has changed
         onView(withId(R.id.errorText))
+                .check(matches(isDisplayed()))
                 .check(matches(withText("Invalid email address")));
     }
 
